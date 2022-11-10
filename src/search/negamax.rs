697edc05d0d6fa966e6a7bd2f292ea708e943221
JacquesRW <jamie.whiting2003@gmail.com>
2022-11-10 23:57:27 +0000

    futility pruning not good at longer time controls

--- src/search/negamax.rs
+++ src/search/negamax.rs
@@ -145,12 +145,6 @@ impl Engine {
             let do_lmr = can_do_lmr::<ROOT>(king_in_check, m_idx, m_score, check);
             let reduction = do_lmr as i8 * (1 + (depth > 8) as i8);
 
-            // futility pruning
-            if !PV && depth - reduction <= 0 {
-                self.board.unmake_move();
-                continue;
-            }
-
             // pvs framework
             // relies on good move ordering!
             let score = if m_idx == 0 {
